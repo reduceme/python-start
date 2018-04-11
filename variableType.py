a5128cde2089905df006df67e856eb7e4c82e3c3
regdai <1148440350@qq.com>
2018-04-11 14:21:05 +0800

    变量类型

---
+++ variableType.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+# number 数字
+number1 = 1
+number2 = 2
+print(number1)
+print(number2)
+del number1
+del number2
+
+# 字符串
+str = 'hello,world'
+print(str)
+print str           # 输出完整字符串
+print str[0]        # 输出字符串中的第一个字符
+print str[2:5]      # 输出字符串中第三个至第五个之间的字符串
+print str[2:]       # 输出从第三个字符开始的字符串
+print str * 2       # 输出字符串两次
+print str + "TEST"  # 输出连接的字符串
+print str[-1]       # 输出最右边的字符
+
+# 列表
+list = ['miuyoga', 786, 2.23, 'miu', 70.2]
+tinylist = [123, 'test']
+
+print list  # 输出完整列表
+print list[0]  # 输出列表的第一个元素
+print list[1:3]  # 输出第二个至第三个元素
+print list[2:]  # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2  # 输出列表两次
+print list + tinylist  # 打印组合的列表
+
+# 元组
+# 元组用"()"标识。内部元素用逗号隔开。但是元组不能二次赋值，相当于只读列表。
+tuple = ('runoob', 786, 2.23, 'john', 70.2)
+tinytuple = (123, 'john')
+
+print tuple  # 输出完整元组
+print tuple[0]  # 输出元组的第一个元素
+print tuple[1:3]  # 输出第二个至第三个的元素
+print tuple[2:]  # 输出从第三个开始至列表末尾的所有元素
+print tinytuple * 2  # 输出元组两次
+print tuple + tinytuple  # 打印组合的元组
+
+# 字典
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print dict['one']  # 输出键为'one' 的值
+print dict[2]  # 输出键为 2 的值
+print tinydict  # 输出完整的字典
+print tinydict.keys()  # 输出所有键
+print tinydict.values()  # 输出所有值
